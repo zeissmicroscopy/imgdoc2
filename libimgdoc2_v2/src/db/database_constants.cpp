62c14d8ddda7d73f567fc9cbecd4e61eeee38407
ptahmose <jbohl@h-quer.de>
2022-09-04 14:40:46 +0200

    tidy: more const

--- libimgdoc2_v2/src/db/database_constants.cpp
+++ libimgdoc2_v2/src/db/database_constants.cpp
@@ -3,32 +3,32 @@
 
 using namespace std;
 
-/*static*/const char* DbConstants::kGeneralTable_Name = "GENERAL";
+/*static*/const char* const DbConstants::kGeneralTable_Name = "GENERAL";
 
-/*static*/const char* DbConstants::kGeneralTable_KeyColumnName = "Key";
-/*static*/const char* DbConstants::kGeneralTable_ValueStringColumnName = "ValueString";
+/*static*/const char* const DbConstants::kGeneralTable_KeyColumnName = "Key";
+/*static*/const char* const DbConstants::kGeneralTable_ValueStringColumnName = "ValueString";
 
-/*static*/const char* DbConstants::kTilesInfoTable_DefaultName = "TILESINFO";
-/*static*/const char* DbConstants::kTilesDataTable_DefaultName = "TILESDATA";
+/*static*/const char* const DbConstants::kTilesInfoTable_DefaultName = "TILESINFO";
+/*static*/const char* const DbConstants::kTilesDataTable_DefaultName = "TILESDATA";
 
-/*static*/const char* DbConstants::kTilesDataTable_Column_Pk_DefaultName = "Pk";
-/*static*/const char* DbConstants::kTilesDataTable_Column_PixelWidth_DefaultName = "PixelWidth";
-/*static*/const char* DbConstants::kTilesDataTable_Column_PixelHeight_DefaultName = "PixelHeight";
-/*static*/const char* DbConstants::kTilesDataTable_Column_PixelType_DefaultName = "PixelType";
-/*static*/const char* DbConstants::kTilesDataTable_Column_TileDataType_DefaultName = "TileDataType";
-/*static*/const char* DbConstants::kTilesDataTable_Column_BinDataStorageType_DefaultName = "BinDataStorageType";
-/*static*/const char* DbConstants::kTilesDataTable_Column_BinDataId_DefaultName = "BinDataId";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_Pk_DefaultName = "Pk";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_PixelWidth_DefaultName = "PixelWidth";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_PixelHeight_DefaultName = "PixelHeight";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_PixelType_DefaultName = "PixelType";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_TileDataType_DefaultName = "TileDataType";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_BinDataStorageType_DefaultName = "BinDataStorageType";
+/*static*/const char* const DbConstants::kTilesDataTable_Column_BinDataId_DefaultName = "BinDataId";
 
-/*static*/const char* DbConstants::kTilesInfoTable_Column_Pk_DefaultName = "Pk";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_TileX_DefaultName = "TileX";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_TileY_DefaultName = "TileY";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_TileW_DefaultName = "TileW";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_TileH_DefaultName = "TileH";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_PyramidLevel_DefaultName = "PyramidLevel";
-/*static*/const char* DbConstants::kTilesInfoTable_Column_TileDataId_DefaultName = "TileDataId";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_Pk_DefaultName = "Pk";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_TileX_DefaultName = "TileX";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_TileY_DefaultName = "TileY";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_TileW_DefaultName = "TileW";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_TileH_DefaultName = "TileH";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_PyramidLevel_DefaultName = "PyramidLevel";
+/*static*/const char* const DbConstants::kTilesInfoTable_Column_TileDataId_DefaultName = "TileDataId";
 
-/*static*/const char* DbConstants::kDimensionColumnPrefix_Default = "Dim_";
-/*static*/const char* DbConstants::kIndexForDimensionColumnPrefix_Default = "IndexForDim_";
+/*static*/const char* const DbConstants::kDimensionColumnPrefix_Default = "Dim_";
+/*static*/const char* const DbConstants::kIndexForDimensionColumnPrefix_Default = "IndexForDim_";
 
 /*static*/const char* DbConstants::GetGeneralTable_ItemKey(GeneralTableItems item)
 {
--- libimgdoc2_v2/src/db/database_constants.h
+++ libimgdoc2_v2/src/db/database_constants.h
@@ -13,33 +13,33 @@ enum class GeneralTableItems
 class DbConstants
 {
 public:
-    static const char* kGeneralTable_Name;                  // = "GENERAL"
+    static const char* const kGeneralTable_Name;                  // = "GENERAL"
 
-    static const char* kGeneralTable_KeyColumnName;         // = "Key"
-    static const char* kGeneralTable_ValueStringColumnName; // = "ValueString"
+    static const char* const kGeneralTable_KeyColumnName;         // = "Key"
+    static const char* const kGeneralTable_ValueStringColumnName; // = "ValueString"
 
-    static const char* kTilesInfoTable_DefaultName;         // = "TILESINFO"
-    static const char* kTilesDataTable_DefaultName;         // = "TILESDATA"
+    static const char* const kTilesInfoTable_DefaultName;         // = "TILESINFO"
+    static const char* const kTilesDataTable_DefaultName;         // = "TILESDATA"
 
-    static const char* kTilesDataTable_Column_Pk_DefaultName;
-    static const char* kTilesDataTable_Column_PixelWidth_DefaultName;
-    static const char* kTilesDataTable_Column_PixelHeight_DefaultName;
-    static const char* kTilesDataTable_Column_PixelType_DefaultName;
-    static const char* kTilesDataTable_Column_TileDataType_DefaultName;
-    static const char* kTilesDataTable_Column_BinDataStorageType_DefaultName;
-    static const char* kTilesDataTable_Column_BinDataId_DefaultName;
+    static const char* const kTilesDataTable_Column_Pk_DefaultName;
+    static const char* const kTilesDataTable_Column_PixelWidth_DefaultName;
+    static const char* const kTilesDataTable_Column_PixelHeight_DefaultName;
+    static const char* const kTilesDataTable_Column_PixelType_DefaultName;
+    static const char* const kTilesDataTable_Column_TileDataType_DefaultName;
+    static const char* const kTilesDataTable_Column_BinDataStorageType_DefaultName;
+    static const char* const kTilesDataTable_Column_BinDataId_DefaultName;
 
 
-    static const char* kTilesInfoTable_Column_Pk_DefaultName;
-    static const char* kTilesInfoTable_Column_TileX_DefaultName;
-    static const char* kTilesInfoTable_Column_TileY_DefaultName;
-    static const char* kTilesInfoTable_Column_TileW_DefaultName;
-    static const char* kTilesInfoTable_Column_TileH_DefaultName;
-    static const char* kTilesInfoTable_Column_PyramidLevel_DefaultName;
-    static const char* kTilesInfoTable_Column_TileDataId_DefaultName;
+    static const char* const kTilesInfoTable_Column_Pk_DefaultName;
+    static const char* const kTilesInfoTable_Column_TileX_DefaultName;
+    static const char* const kTilesInfoTable_Column_TileY_DefaultName;
+    static const char* const kTilesInfoTable_Column_TileW_DefaultName;
+    static const char* const kTilesInfoTable_Column_TileH_DefaultName;
+    static const char* const kTilesInfoTable_Column_PyramidLevel_DefaultName;
+    static const char* const kTilesInfoTable_Column_TileDataId_DefaultName;
 
-    static const char* kDimensionColumnPrefix_Default;  // = "Dim_"
-    static const char* kIndexForDimensionColumnPrefix_Default; // = "IndexForDim_"
+    static const char* const kDimensionColumnPrefix_Default;  // = "Dim_"
+    static const char* const kIndexForDimensionColumnPrefix_Default; // = "IndexForDim_"
 
 
     static const char* GetGeneralTable_ItemKey(GeneralTableItems item);
