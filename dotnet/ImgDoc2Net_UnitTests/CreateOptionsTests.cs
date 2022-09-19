950ecd5dc075ccaae01275f3e4dc2f8a2c1cb0b8
J. Bohl <juergen.bohl@zeiss.com>
2022-09-19 12:06:20 +0200

    add .NET-unittest-project

---
+++ dotnet/ImgDoc2Net_UnitTests/CreateOptionsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ImgDoc2Net_UnitTests
+{
+    using ImgDoc2Net.Interop;
+
+    public class CreateOptionsTests
+    {
+        [Fact]
+        public void SetFilenameAndGetItAndCompareResult()
+        {
+            const string Filename = "TESTTEXT";
+            var instance = ImgDoc2ApiInterop.Instance;
+
+            var handle = instance.CreateCreateOptions();
+            instance.CreateOptionsSetFilename(handle, Filename);
+
+            string s = instance.CreateOptionsGetFilename(handle);
+
+            Assert.Equal(s, Filename);
+            instance.DestroyCreateOptions(handle);
+        }
+
+        [Fact]
+        public void SetFilenameWithUnicodeCharactersAndGetItAndCompareResult()
+        {
+            const string Filename = "ÄÖÜäöüß 馬匹背負著馬鞍";
+
+            var instance = ImgDoc2ApiInterop.Instance;
+            var handle = instance.CreateCreateOptions();
+            instance.CreateOptionsSetFilename(handle, Filename);
+            string s = instance.CreateOptionsGetFilename(handle);
+            Assert.Equal(s, Filename);
+            instance.DestroyCreateOptions(handle);
+        }
+    }
+}
